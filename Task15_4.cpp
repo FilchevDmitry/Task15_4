7f925a8578642b9dd711f1e4606216d5adccf1b0
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-05-15 09:01:41 +0300

    Добавьте файлы проекта.

---
+++ Task15_4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+int main()
+{
+    vector<int> vec = { -100,-50,-5,1,10,15 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		for (int j = 0; j < vec.size()-1; j++)
+		{
+
+			if (abs(vec[j]) > abs(vec[j+1]))
+			{
+				int temp = vec[j+1];
+				vec[j+1] = vec[j];
+				vec[j] = temp;
+			}
+			
+		}
+	}
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec[i] << " ";
+	}
+}
+
